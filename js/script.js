3e7b902f682ed195bfc23a19c2cae1b3dacdfe99
HamzaBraik01 <hamzabraik02@gmail.com>
2024-11-29 00:22:42 +0100

    add validation form

--- js/script.js
+++ js/script.js
@@ -1,116 +1,3 @@
-/*document.addEventListener("DOMContentLoaded", () => {
-    const allPlayersContainer = document.querySelector(".all-players");
-  
-    
-    fetch("players.json")
-    .then((response) => response.json())
-    .then((data) => {
-      const players = data.players; 
-      displayAllPlayers(players);
-    })
-    .catch((error) => console.error("Erreur lors du chargement des joueurs :", error));
-  
-    
-    function displayAllPlayers(players) {
-      allPlayersContainer.innerHTML = "";
-  
-      
-      const categories = groupByCategory(players);
-  
-      
-      for (const category in categories) {
-        
-        const categoryContainer = document.createElement("div");
-        categoryContainer.classList.add("category-container");
-  
-        categoryContainer.innerHTML = `<h2 class="category-title">${category}</h2>`;
-  
-        
-        const playerGrid = document.createElement("div");
-        playerGrid.classList.add("player-grid");
-  
-        categories[category].forEach((player) => {
-          const playerBadge = createPlayerBadge(player);
-          playerGrid.appendChild(playerBadge);
-        });
-  
-        categoryContainer.appendChild(playerGrid);
-        allPlayersContainer.appendChild(categoryContainer);
-      }
-    }
-  
-    
-    function groupByCategory(players) {
-      return players.reduce((acc, player) => {
-        if (!acc[player.position]) {
-          acc[player.position] = [];
-        }
-        acc[player.position].push(player);
-        return acc;
-      }, {});
-    }
-  
-    
-function createPlayerBadge(player) {
-    const playerBadge = document.createElement("div");
-      playerBadge.classList.add("player-badge-gold");
-  
-      playerBadge.innerHTML = `
-      <div class="badge">
-          <img src="./img/badge_gold.webp" alt="Badge" class="badge-bg">
-          <div class="player-info">
-            <img src="${player.photo}" alt="${player.name}" class="player-photo">
-            <h3 class="player-name">${player.name}</h3>
-            <p class="player-position">${player.position}</p>
-            <p class="player-club">
-              <img src="${player.logo}" alt="${player.club}" class="club-logo">
-              ${player.club}
-            </p>
-          </div>
-        </div>
-    
-      `;
-  
-      return playerBadge;
-    }
-  });
-*/
-
-/*
-// Récupération des éléments du DOM
-const addPlayerBtn = document.getElementById('addPlayerBtn');
-const modal = document.getElementById('modal');
-const closeModal = document.getElementById('closeModal');
-const playerForm = document.getElementById('playerForm');
-
-// Afficher le modal au clic du bouton
-addPlayerBtn.addEventListener('click', () => {
-    modal.style.display = 'flex';
-});
-
-// Fermer le modal au clic sur la croix
-closeModal.addEventListener('click', () => {
-    modal.style.display = 'none';
-});
-
-// Gestion du formulaire
-playerForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-    const newPlayer = {
-        name: document.getElementById('name').value,
-        photo: document.getElementById('photo').value,
-        position: document.getElementById('position').value,
-        rating: document.getElementById('rating').value,
-    };
-
-    console.log('Nouveau joueur ajouté:', newPlayer);
-
-  // Réinitialise le formulaire
-    playerForm.reset();
-    modal.style.display = 'none';
-});
-*/
-
 const addPlayerBtn = document.getElementById('addPlayerBtn');
 const modal = document.getElementById('modal');
 const closeModal = document.getElementById('closeModal');
@@ -133,57 +20,113 @@ window.addEventListener('click', (e) => {
 
 positionSelect.addEventListener('change', () => {
     const position = positionSelect.value;
-    dynamicFields.innerHTML = ''; // Réinitialise les champs
+    dynamicFields.innerHTML = ''; 
 
     if (position === 'GK') {
     dynamicFields.innerHTML = `
         <div class="form-group">
             <label for="diving">Diving :</label>
-            <input type="number" id="diving" name="diving" placeholder="Entrez la note de plonger" required>
+            <input type="number" id="diving" name="diving" placeholder="Entrez la note de plonger" r>
 
             <label for="handling">Handling :</label>
-            <input type="number" id="handling" name="handling" placeholder="Entrez la note de maniement" required>
+            <input type="number" id="handling" name="handling" placeholder="Entrez la note de maniement" >
         </div>
         <div class="form-group">
             <label for="kicking">Kicking :</label>
-            <input type="number" id="kicking" name="kicking" placeholder="Entrez la note de dégagement" required>
+            <input type="number" id="kicking" name="kicking" placeholder="Entrez la note de dégagement" >
 
             <label for="reflexes">Reflexes :</label>
-            <input type="number" id="reflexes" name="reflexes" placeholder="Entrez la note des réflexes" required>
+            <input type="number" id="reflexes" name="reflexes" placeholder="Entrez la note des réflexes" >
         </div>
         <div class="form-group">
             <label for="speed">Speed :</label>
-            <input type="number" id="speed" name="speed" placeholder="Entrez la note de vitesse" required>
+            <input type="number" id="speed" name="speed" placeholder="Entrez la note de vitesse" >
 
             <label for="positioning">Positioning :</label>
-            <input type="number" id="positioning" name="positioning" placeholder="Entrez la note de positionnement" required>
+            <input type="number" id="positioning" name="positioning" placeholder="Entrez la note de positionnement" >
         </div>
     `;
 } else {
     dynamicFields.innerHTML = `
         <div class="form-group">
             <label for="pace">Pace :</label>
-            <input type="number" id="pace" name="pace" placeholder="Entrez la note de vitesse" required>
+            <input type="number" id="pace" name="pace" placeholder="Entrez la note de vitesse" >
 
             <label for="shooting">Shooting :</label>
-            <input type="number" id="shooting" name="shooting" placeholder="Entrez la note de tir" required>
+            <input type="number" id="shooting" name="shooting" placeholder="Entrez la note de tir" >
         </div>
         <div class="form-group">
             <label for="passing">Passing :</label>
-            <input type="number" id="passing" name="passing" placeholder="Entrez la note de passe" required>
+            <input type="number" id="passing" name="passing" placeholder="Entrez la note de passe" >
 
             <label for="dribbling">Dribbling :</label>
-            <input type="number" id="dribbling" name="dribbling" placeholder="Entrez la note de dribble" required>
+            <input type="number" id="dribbling" name="dribbling" placeholder="Entrez la note de dribble" >
         </div>
         <div class="form-group">
             <label for="defending">Defending :</label>
-            <input type="number" id="defending" name="defending" placeholder="Entrez la note de défense" required>
+            <input type="number" id="defending" name="defending" placeholder="Entrez la note de défense" >
 
             <label for="physical">Physical :</label>
-            <input type="number" id="physical" name="physical" placeholder="Entrez la note de physique" required>
+            <input type="number" id="physical" name="physical" placeholder="Entrez la note de physique" >
         </div>
     `;
 }
 
 });
+const playerForm = document.getElementById('playerForm');
+playerForm.addEventListener('submit', (e) => {
+    e.preventDefault(); 
+
+    const name = document.getElementById('name').value.trim();
+    const photo = document.getElementById('photo').value.trim();
+    const nationality = document.getElementById('nationality').value.trim();
+    const club = document.getElementById('club').value.trim();
+    const logo = document.getElementById('logo').value.trim();
+    const rating = document.getElementById('rating').value.trim();
+
+    
+    const nameRegex = /^[a-zA-Z\s-]+$/; 
+    const urlRegex = /^(https?:\/\/[^\s]+)$/; 
+    const ratingRegex = /^([1-9][0-9]?|100)$/; 
+
+    
+    if (!nameRegex.test(name)) {
+        alert('Le nom doit contenir uniquement des lettres, espaces ou tirets.');
+        return;
+    }
+    if (!urlRegex.test(photo)) {
+        alert('La photo doit être une URL valide.');
+        return;
+    }
+    if (!nameRegex.test(nationality)) {
+        alert('La nationalité doit contenir uniquement des lettres, espaces ou tirets.');
+        return;
+    }
+    if (!nameRegex.test(club)) {
+        alert('Le nom du club doit contenir uniquement des lettres, espaces ou tirets.');
+        return;
+    }
+    if (!urlRegex.test(logo)) {
+        alert('Le logo doit être une URL valide.');
+        return;
+    }
+    if (!ratingRegex.test(rating)) {
+        alert('La note générale doit être un nombre entre 1 et 100.');
+        return;
+    }
+
+
+    const dynamicInputs = document.querySelectorAll('#dynamicFields input[type="number"]');
+    for (let input of dynamicInputs) {
+        const value = input.value.trim();
+        if (!ratingRegex.test(value)) {
+            alert(`${input.name} doit être une note valide entre 1 et 100.`);
+            return;
+        }
+    }
+
+    alert('Formulaire soumis avec succès !');
+    playerForm.reset(); 
+});
+
 
